cef582639745cadae7bec5d09154a9985f8c0568
Thurzinzkq <alves.mello.arthur@escola.pr.gov.br>
2026-05-22 11:17:16 +0000

    v1

---
+++ ex4.py
@@ -0,0 +1,10 @@
+temperatura = 35
+if temperatura > 30:
+    print("Calor! Beba água.")
+    else:
+        print("Temperatura ok.")
+        numero= -5
+        if numero >= 0:
+            print("Positivo ou zero")
+            else:
+                print("Negativo")
