2dfa30dbad4b9a3d666a22eca78318e0ec4a7216
Thurzinzkq <alves.mello.arthur@escola.pr.gov.br>
2026-05-22 11:54:33 +0000

    v1

---
+++ ex6.py
@@ -0,0 +1,12 @@
+nota float(input("Nota (0 a 10): "))
+if nota >= 9:
+    print("Conceito A Excelente!")
+elif nota >= 7:
+    print("Conceito B Bom")
+    elif nota >= 5:
+        print("Conceito C Regular")
+        else:
+            print("Conceito D Insuficiente")
+            if nota >= 6:
+                print("Aprovado ")
+            
